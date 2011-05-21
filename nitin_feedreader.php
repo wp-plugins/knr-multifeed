8534cc78f7b09f7c301fae033617872e1aa8c51d
k_nitin_r <k_nitin_r@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-21 20:01:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/knr-multifeed/trunk@387505 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nitin_feedreader.php
+++ nitin_feedreader.php
@@ -1,4 +1,30 @@
 <?php
+/*
+-=[ Copyright Notice ]=-
+
+    Copyright 2009 Nitin Reddy  (email : k_nitin_r {at} antispamyahoo.co.in , k.nitin.r {at} antispamgmail.com)
+                                    Replace the {at} with @ and remove the antispam for my email address
+                                    
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	
+	WARRANTY AND CUSTOMIZATION
+	Warranty and customization for this software is available. Contact the
+	author for more details.
+*/
+
 define(FEEDURL, 'http://feeds.feedburner.com/KrishnaConnect?format=xml');
 define(LOCALFILE, 'localfile.xml');
 
